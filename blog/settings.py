9966aeb8f7976a7e504114f16151014f8780d648
Michael Altschuler <altschm1@tcnj.edu>
2021-07-12 13:35:19 -0400

    deployment

--- blog/settings.py
+++ blog/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+import os
 STATIC_URL = '/static/'
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
