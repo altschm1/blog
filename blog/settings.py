1880172599db00c38c68d8f6700fb44971ec13a3
Michael Altschuler <altschm1@tcnj.edu>
2021-07-12 13:53:24 -0400

    updated allowed_hosts with site

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://justsomefunstuff.herokuapp.com"]
+ALLOWED_HOSTS = ["justsomefunstuff.herokuapp.com"]
 
 
 # Application definition
